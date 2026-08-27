2d9647c7da1403d019a3f7d70b1202f8c002f3a0
MehranBazrafkan <mhrn.bzrafkn.dev@gmail.com>
2026-08-27 02:38:48 +0200

    Add calculator

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html"], // make sure your templates are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
